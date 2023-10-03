c4fcd1933a9a4875ec4491563b418c38b46a3e85
lyccyc <you@example.com>
2023-10-03 19:43:54 +0800

    tired

--- lab/2023.09.25-2.c
+++ lab/2023.09.25-2.c
@@ -9,8 +9,8 @@ int main() {
 
   while (n > 0) {
     reminder=n%10;
-    reverse = reverse * 10 + reminder;
-    n /= 10;    
+    reverse = reverse * 10 + reminder; //40+2=43   430+2=432 4320+1=4321
+    n /= 10;    //1
   }
 
   printf("%04d\n", reverse);
