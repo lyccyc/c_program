fd49b4cd40194837ee0955b888771a71ae9fbb10
lyccyc <you@example.com>
2023-10-11 09:40:08 +0800

    multiplication

---
+++ programming/mutiplication.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+int x=1,y=1;
+
+for (int i=1;i<=81;i++){
+    printf("%d x %d= %2d",x,y,x*y);
+    if (i%27==0){
+        x+=1;
+        y=1;
+        printf("\n\n");
+    }
+
+    else if(i%3==0){
+        x=x-2;
+        y+=1;
+        printf("\n");
+    }
+
+    else{
+        x+=1;
+        printf("    ");
+    }
+}
+}
